cfe6849ba5a681c872c2df12adc7d9f152b1dbe9
Alexey_Volkov <alexiy87@inbox.ru>
2023-02-19 01:46:54 +0300

    feat:add new files in homework

---
+++ Task001/Program.cs
@@ -0,0 +1,62 @@
+﻿/*"Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+int GetNumber (string message)
+{   
+    int result = 0;
+    System.Console.WriteLine(message);
+
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else System.Console.WriteLine("Неверный ввод, повторите");
+    }
+    return result;
+}
+
+int [] InitArray (int result)
+{
+        int []array = new int [result];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array [i] = GetNumber ("Введите число с индексом "+ i);
+        }
+
+    return array;
+}
+
+void PrintArray (int []array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+int FindQtyOfNumbers (int []array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int number = GetNumber("Введите размер массива");
+int [] array = InitArray (number);
+
+int count = FindQtyOfNumbers(array);
+
+System.Console.WriteLine("Чисел больше 0 = "+ count);
---
+++ Task002/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetNumber (string message)
+{
+    double number = 0;
+    System.Console.WriteLine(message);
+
+    while (true)
+    {
+        if(double.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        else System.Console.WriteLine("Неверный ввод, повторите");
+    }
+    return number;
+}
+
+void FindIntersectionPoint (double k1, double k2, double b1,double b2)
+{
+
+    if (k1!=k2)
+    {
+        double result = (b2-b1)/(k1-k2);
+        System.Console.WriteLine("Точка пересечения " + result);
+    }
+    else System.Console.WriteLine("Нет точек пересечения ");
+    
+}
+
+double b1 = GetNumber("Введите координаты b1");
+double b2 = GetNumber("Введите координаты b2");
+double k1 = GetNumber("Введите координаты k1");
+double k2 = GetNumber("Введите координаты k2");
+FindIntersectionPoint (k1,k2,b1,b2);
+// System.Console.WriteLine("Точка пересечения" + result);
